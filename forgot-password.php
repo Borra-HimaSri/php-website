27e5347b73e5504e966f7308dfb3c645202664b1
Borra-HimaSri <himasri_csd@srkrec.edu.in>
2025-08-02 11:10:12 +0530

    k

--- forgot-password.php
+++ forgot-password.php
@@ -5,7 +5,7 @@ error_reporting(E_ALL);
 ini_set('display_errors', 1);
 
 // Database connection
-$conn = new mysqli($servername, $username, $password, $dbname);
+
 if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
 }
